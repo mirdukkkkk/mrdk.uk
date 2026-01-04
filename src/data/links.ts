b11846bf8f61edfa5d3bc98c577fd07cd4517d34
RΞIKKA <69540930+mirdukkkkk@users.noreply.github.com>
2026-01-05 01:35:46 +0300

    Update links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,9 +1,5 @@
-const makeLink = (subdomain: string) => `https://${subdomain}.mirdukkkkk.space`;
-
 export default [
-    [makeLink("life"), "My Pixel Battle"],
-    [makeLink("source"), "This website repo"],
-    [makeLink("github"), "GitHub"],
-    [makeLink("discord"), "Discord"],
-    [makeLink("youtube"), "YouTube"]
+    ["https://pixelbattle.fun", "MY PIXEL BATTLE"],
+    ["https://github.com/mirdukkkkk/mrdk.uk", "MRDK.UK REPO"],
+    ["https://github.com/mirdukkkkk", "GITHUB"]
 ];
